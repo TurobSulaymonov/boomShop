39b0065332f102f5b09e78a1aad6da2bf811db81
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-04-19 02:32:12 +0900

    feat: develop dynamic title

--- routers/auth.js
+++ routers/auth.js
@@ -2,11 +2,15 @@ import {Router} from 'express';
 const router = Router();
 router.get("/login", (req, res) => {
     //res.sendFile(path.join(__dirname, "views", "about.html"));
-    res.render("login")
+    res.render("login" ,{
+        title: `Login | Turob`
+    })
 });
 router.get("/register", (req, res) => {
     //res.sendFile(path.join(__dirname, "views", "about.html"));
-    res.render("register")
+    res.render("register",{
+        title: `Register | Turob`
+    })
 });
 
 export default router;
--- routers/products.js
+++ routers/products.js
@@ -1,15 +1,22 @@
 import {Router} from 'express';
 const router = Router();
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index",{
+        title: `BoomShop | Turob`
+    });
   });
   
   router.get("/products", (req, res) => {
       //res.sendFile(path.join(__dirname, "views", "about.html"));
-      res.render("products")
+      res.render("products",{
+        title: `Products | Turob`,
+        isProducts: true,
+    })
   });
   router.get("/add", (req, res) => {
       //res.sendFile(path.join(__dirname, "views", "about.html"));
-      res.render("add")
+      res.render("add", {
+        title: `Add | Turob`
+    })
   });
   export default router;
