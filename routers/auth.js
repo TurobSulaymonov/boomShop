9d994d7cdde15842c46fc35e3f1c21b41201ac27
TurobSulaymonov <turobsulaymonov117@gmail.com>
2024-12-31 07:30:59 +0900

    fix: modify register & login api and develop hashing user password business logic

--- routers/auth.js
+++ routers/auth.js
@@ -1,5 +1,8 @@
 import {Router} from 'express';
-import User from '../models/Users.js'
+import User from '../models/Users.js';
+import bcrypt from 'bcrypt'
+
+
 const router = Router();
 router.get("/login", (req, res) => {
     res.render("login" ,{
@@ -11,17 +14,42 @@ router.get("/register", (req, res) => {
         title: `Register | Turob`
     })
 });
-router.post("/login", (req, res) => {
-    console.log(req.body)
-    res.redirect("/" )
+
+
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body
+
+	if (!email || !password) {
+		req.flash('loginError', 'All fields is required')
+		res.redirect('/login')
+		return
+	}
+
+	const existUser = await User.findOne({email})
+	if (!existUser) {
+		req.flash('loginError', 'User not found')
+		res.redirect('/login')
+		return
+	}
+
+	const isPassEqual = await bcrypt.compare(password, existUser.password)
+	if (!isPassEqual) {
+		req.flash('loginError', 'Password wrong')
+		res.redirect('/login')
+		return
+	}
+
+	
+	res.redirect('/')
 });
 
 router.post("/register", async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
     const userData = {
         firstName: req.body.firstname,
         lastName: req.body.lastname,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     }
    const user = await User.create(userData);
     console.log(req.body)
